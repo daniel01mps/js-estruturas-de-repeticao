7786383e770e9c6af337e30f267e781718ccbe9c
daniel01mps <daniel01mps@gmail.com>
2024-10-22 22:15:08 -0300

    Atividades

---
+++ index.js
@@ -0,0 +1,42 @@
+// let numero = prompt ('Digite um numero')
+// let total = 0
+//     for (let i = 1; i <= numero; i++) {
+//         total += i 
+// }        
+// document.write('A soma dos numeros é ' + total)
+
+// let numero = prompt ('Digite um número')
+// let numeroImpar = []
+
+// for (let i = 1; i <= numero; i++){
+
+// if (i % 2 != 0){
+//     numeroImpar.push(i)
+// }
+// }   
+// document.write (`O numeros impares são ${numeroImpar}`)
+
+// let numero = prompt ('Digite um número')
+// let numeroFatorial = 1
+// for (let i = 1; i <= numero; i++) {
+//     numeroFatorial *= i; 
+// }
+// document.write('O fatorial de ' + numero + ' é ' + numeroFatorial);
+
+
+// let numero = prompt('Digite um número')
+// let divisores = 0;
+// for(let i = 1; i <= numero;i++){
+//     if(numero % i == 0){
+//         divisores++; 
+// }}
+
+// if(divisores == 2){
+//     document.write(`O ${numero} é primo `)
+// }else if(divisores > 2){
+//     document.write(`O ${numero} não é primo `)
+// }
+
+let numero = prompt ('Digite um número')
+for (let i = 1; i <= 10; i++) {
+document.write(`${numero} x ${i} = ${numero * i} <br>`)}
